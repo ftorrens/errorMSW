53c6c947cefa55003fe345ed5792b404053f3ccd
fernandotorrensabeal <fernando.torrens@grupokonecta.com>
2023-07-18 14:31:07 +0200

    UPLOAD INDEX

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,20 +1,53 @@
 <template>
 
-  <h1>VUE PROYECT WITH MSW</h1>
+  <h1>Switch using Vee-Validate</h1>
+
+  <k-switch
+    id="legal-integer"
+    v-model="data.form.legal_integer"
+    label="Accept Integer"
+    name="legal"
+    response-format="Integer"
+    rules="required"
+  />
+
+  <k-switch
+    id="legal-boolean"
+    v-model="data.form.legal_boolean"
+    label="Accept Boolean"
+    name="legal"
+    rules="required"
+  />
+
 
 </template>
 
 <script>
 
+import { ref } from "vue";
 import Package from '/package.json'
+import KSwitch from '../components/switch.vue'
 
 export default {
 
   name: 'IndexPage',
 
+  components: {
+    'k-switch': KSwitch
+  },
+
   setup() {
+
+    const data = ref({
+      form: {
+        legal_integer: 0,
+        legal_boolean: false,
+      }
+    })
+
     return {
-      Package
+      Package,
+      data
     }
   }
 }
